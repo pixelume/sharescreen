12a8687ec0796eff80aadb18780690bfdb44f93e
Pieter Wolmarans <pieter@pixelume.com>
2021-08-12 15:59:56 +0200

    Changed intro video on homepage

--- src/components/Layout/Blocks/VideoWithHeading.js
+++ src/components/Layout/Blocks/VideoWithHeading.js
@@ -49,7 +49,7 @@ const VideoWithHeading = () => {
             width: '100%',
             height: '100%',
           }}
-          src='https://www.youtube.com/embed/vulhF15fTDk'
+          src='https://www.youtube.com/embed/dY8yU_DHsaA'
           title='YouTube video player'
           frameBorder={0}
           allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture'
